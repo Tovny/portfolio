fdf9c2ca733038d4a76142a94f012a1c8b828271
Anton Drofenik <a93.dro@gmai.com>
2021-05-19 17:36:18 +0200

    added projects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 
 import LandingPage from "./components/landingPage/LandingPage";
+import Projects from "./components/projects/Projects";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <Projects />
     </div>
   );
 }
