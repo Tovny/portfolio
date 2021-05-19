48f4b66c0dcd057cebaeae4d7a1999405481a858
Anton Drofenik <a93.dro@gmai.com>
2021-05-19 17:37:01 +0200

    created project component

---
+++ src/components/projects/Project.js
@@ -0,0 +1,12 @@
+const Project = () => {
+  return (
+    <div className="project">
+      <div className="sitePreviews">
+        <div className="mobileSite"></div>
+        <div className="desktopSite"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
