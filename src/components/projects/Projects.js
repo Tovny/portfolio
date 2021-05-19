c9f57481e7cfd668a106cb80672613a261f48416
Anton Drofenik <a93.dro@gmai.com>
2021-05-19 17:36:51 +0200

    created projects page

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,14 @@
+import Project from "./Project";
+
+import "./Projects.scss";
+
+const Projects = () => {
+  return (
+    <section className="projects">
+      <Project />
+      <Project />
+    </section>
+  );
+};
+
+export default Projects;
