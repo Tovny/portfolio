13f6b55f149ed8154bf53ddcf2144311ef0f22c9
Anton Drofenik <a93.dro@gmai.com>
2021-05-22 16:42:40 +0200

    added project descriptions

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,10 +1,63 @@
-const Project = () => {
+import { FaGithub } from "react-icons/fa";
+
+const Project = ({
+  preview,
+  imgPos,
+  tech,
+  title,
+  description,
+  href,
+  github,
+}) => {
   return (
-    <div className="project">
-      <div className="sitePreviews">
-        <div className="mobileSite"></div>
-        <div className="desktopSite"></div>
+    <div
+      className="project"
+      style={imgPos === "left" ? { textAlign: "left" } : { textAlign: "right" }}
+    >
+      {imgPos === "left" ? (
+        <div className="sitePreviews">
+          <img src={preview} alt="prev"></img>
+        </div>
+      ) : null}
+
+      <div className="projectDesc">
+        <h4>{tech}</h4>
+        <h2>{title}</h2>
+        {description}
+        <div
+          className="projectLinks"
+          style={
+            imgPos === "left"
+              ? { justifyContent: "flex-start" }
+              : { justifyContent: "flex-end" }
+          }
+        >
+          {imgPos === "left" ? (
+            <>
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                <h3>Live Demo</h3>
+              </a>
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+              </a>
+            </>
+          ) : (
+            <>
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                <FaGithub />
+              </a>
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                <h3>Live Demo</h3>
+              </a>
+            </>
+          )}
+        </div>
       </div>
+      {imgPos === "right" ? (
+        <div className="sitePreviews">
+          <img src={preview} alt="prev"></img>
+        </div>
+      ) : null}
     </div>
   );
 };
