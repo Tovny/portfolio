c4242c68e5fdc21ea97a18b35ecd539b998f08e8
Anton Drofenik <a93.dro@gmai.com>
2021-05-24 13:30:17 +0200

    added gsap animations

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,3 +1,8 @@
+import { useEffect, useRef } from "react";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import { FaGithub } from "react-icons/fa";
 
 const Project = ({
@@ -9,18 +14,49 @@ const Project = ({
   href,
   github,
 }) => {
+  const projectDiv = useRef();
+  const projectDescDiv = useRef();
+  const projectPreviewImg = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      scrollTrigger: { trigger: projectDiv.current, start: "center bottom" },
+    });
+
+    tl.from(projectPreviewImg.current, {
+      opacity: 0,
+      x: imgPos === "left" ? -100 : 100,
+      duration: 1.5,
+    }).from(
+      projectDescDiv.current.children,
+      {
+        opacity: 0,
+        x: imgPos === "left" ? 100 : -100,
+        duration: 0.75,
+        stagger: 0.25,
+      },
+      "-= 1"
+    );
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div
-      className="project"
+      className={imgPos === "left" ? "project" : "project right"}
       style={imgPos === "left" ? { textAlign: "left" } : { textAlign: "right" }}
+      ref={projectDiv}
     >
-      {imgPos === "left" ? (
-        <div className="sitePreviews">
-          <img src={preview} alt="prev"></img>
-        </div>
-      ) : null}
-
-      <div className="projectDesc">
+      <div className="sitePreviews">
+        <img
+          src={preview}
+          alt={`${title} preview`}
+          ref={projectPreviewImg}
+        ></img>
+      </div>
+      <div className="projectDesc" ref={projectDescDiv}>
         <h4>{tech}</h4>
         <h2>{title}</h2>
         {description}
@@ -35,7 +71,7 @@ const Project = ({
           {imgPos === "left" ? (
             <>
               <a href={href} target="_blank" rel="noopener noreferrer">
-                <h3>Live Demo</h3>
+                <h4>Live Demo</h4>
               </a>
               <a href={github} target="_blank" rel="noopener noreferrer">
                 <FaGithub />
@@ -47,17 +83,12 @@ const Project = ({
                 <FaGithub />
               </a>
               <a href={href} target="_blank" rel="noopener noreferrer">
-                <h3>Live Demo</h3>
+                <h4>Live Demo</h4>
               </a>
             </>
           )}
         </div>
       </div>
-      {imgPos === "right" ? (
-        <div className="sitePreviews">
-          <img src={preview} alt="prev"></img>
-        </div>
-      ) : null}
     </div>
   );
 };
