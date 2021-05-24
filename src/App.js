8469f8df2ee46be8952c08d92373600bf2e65123
Anton Drofenik <a93.dro@gmai.com>
2021-05-24 17:19:37 +0200

    added contact component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.scss";
 
 import LandingPage from "./components/landingPage/LandingPage";
 import Projects from "./components/projects/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
       <Projects />
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import "./Contact.scss";
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <h2>contact</h2>
+    </section>
+  );
+};
+
+export default Contact;
