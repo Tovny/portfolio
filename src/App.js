677eb3724676b5661365bad745bf96f15c2acea9
Anton Drofenik <a93.dro@gmai.com>
2021-05-26 16:38:03 +0200

    added eventlistener for vh changes

--- src/App.js
+++ src/App.js
@@ -13,8 +13,17 @@ function App() {
   const projectSection = useRef();
   const aboutSection = useRef();
 
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
   useEffect(() => {
     smoothscroll.polyfill();
+
+    window.addEventListener("resize", setVh);
+
+    return () => window.removeEventListener("resize", setVh);
   }, []);
   return (
     <div className="App">
